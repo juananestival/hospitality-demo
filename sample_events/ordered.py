9483e2a6167a286eaac20c8a964b229865672a3a
John Doepwd <juananestival@gmail.com>
2022-09-08 22:33:23 +0200

    salesforce query using params

---
+++ sample_events/ordered.py
@@ -0,0 +1,6 @@
+([
+    ('totalSize', 1), 
+    ('done', True), 
+    ('records', [OrderedDict([
+        ('attributes', OrderedDict([
+            ('type', 'Contact'), ('url', '/services/data/v52.0/sobjects/Contact/0035I00000XWZ19QAH')])), ('Id', '0035I00000XWZ19QAH')])])])
--- src/python-function-salesforce/main.py
+++ src/python-function-salesforce/main.py
@@ -20,17 +20,17 @@ def main(initial_request):
     
         # Retrieve salesforce session and instance reference
         session_id, instance = sf_login()
+        sf = Salesforce(instance=instance, session_id=session_id)
+        
         # Get Tag
         tag = request_json['fulfillmentInfo']['tag']
         print(f"tag : {tag}")
-        customer_id = request_json['sessionInfo']['parameters']['customerid']
-        print(f"customer {customer_id}")
+
 
         # Evaluate Tag
         if tag == 'phoneLookup':
-            print(f"Tag flow for: {tag}")
-            whatDoIHave = sf_phoneLookup(initial_request)
-            print(f'Answer from phonelookup {whatDoIHave}')
+
+            whatDoIHave = sf_phoneLookup(initial_request, sf)
 
         else:
             print("Else: ")
@@ -47,11 +47,26 @@ def main(initial_request):
 # Add your functions here
 
 ###############################################################################
-def sf_phoneLookup(initial_request):
+def sf_phoneLookup(initial_request, sf):
+    request_json = initial_request.get_json()
+    param_customer_id = request_json['sessionInfo']['parameters']['customerid']
+    query_string = "SELECT Id FROM Contact  WHERE Phone = '{}'".format(param_customer_id)
+    print(query_string)
+    
     try:
-        request_json = initial_request.get_json()
-        customer_id = request_json['sessionInfo']['session']['parameters']['customerid']
-        print(f"customer {customer_id}")
+        customers_by_phone = sf.query(query_string)
+        print("customers by phone {}".format(customers_by_phone))
+        total_records = customers_by_phone.get("totalSize")
+        if total_records == 1:
+            print("one")
+            customer_records = customers_by_phone.get("records")
+            c_firstItem = customer_records[0]
+            customer_id = c_firstItem["Id"]
+            print (customer_id)
+        elif total_records == 0:
+            print("zero")   
+        else:
+            print("more")
         returnMessage = "Just text"
         return returnMessage
     except Exception as error:
@@ -74,10 +89,8 @@ def answer_webhook(msg):
 
 ###############################################################################
 def sf_login():
-    print("control -1")
     # Create the Secret Manager client.
     client = secretmanager.SecretManagerServiceClient()
-    print("control -2")
     # Organize the Secret Keys
     sf_user_prod = "SF_USER_PROD"
     sf_pass_prod = "SF_PASS_PROD"
@@ -86,35 +99,29 @@ def sf_login():
     # Pass in the GCP Project ID
     # This will be found on the Secret Manager > Secret > Secret Details
     # projects/[gcp_project_id]/secrets/[secret]
-    print("control -3")
     project_id = os.environ["gcp_project_id"]
     
     # Obtain the Secret Name Path
-    print("control -4")
     sf_user_prod_name = f"projects/{project_id}/secrets/{sf_user_prod}/versions/latest"
     sf_pass_prod_name = f"projects/{project_id}/secrets/{sf_pass_prod}/versions/latest"
     sf_token_prod_name = f"projects/{project_id}/secrets/{sf_token_prod}/versions/latest"   
     
     # Obtain the Latest Secret Version
-    print("control -5")
     sf_user_prod_response = client.access_secret_version(sf_user_prod_name)
     sf_pass_prod_response = client.access_secret_version(sf_pass_prod_name)
     sf_token_prod_response = client.access_secret_version(sf_token_prod_name)
 
     # Parse the Secret Response & Decode Payload
-    print("control -6")
     sf_user_prod_secret = sf_user_prod_response.payload.data.decode('UTF-8')  
     sf_pass_prod_secret = sf_pass_prod_response.payload.data.decode('UTF-8') 
     sf_token_prod_secret = sf_token_prod_response.payload.data.decode('UTF-8')     
 
     # Assign Variables to Pass into Salesforce Login
-    print("control -7")
     sf_username = sf_user_prod_secret
     sf_password = sf_pass_prod_secret
     sf_token = sf_token_prod_secret
  
     try:
-        print("control 1")
         # call salesforce Login
         # return Session ID and Instance
         session_id, instance = SalesforceLogin(
@@ -122,17 +129,13 @@ def sf_login():
             password = sf_password,
             security_token = sf_token
             )
-        # c_id = instance.query("SELECT Id FROM Contact WHERE LastName = 'Next201'")
-        print ("session id")
-        print (session_id)
         
-        print ("instance id")
-        print (instance)
+  
         
-        sf = Salesforce(instance=instance, session_id=session_id)
-        c_id = sf.query("SELECT Id FROM Contact WHERE Phone = '+34664004600'")
-        print(c_id)
-        print("control 2")
+ 
+        
+      
+       
 
         return session_id, instance
     
